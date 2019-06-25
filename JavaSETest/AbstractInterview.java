345b2596e0909d300aa376d33c37cf2327b25a19
xiaomessi <921721870@qq.com>
2019-06-25 23:33:55 +0800

    Add some test

--- JavaSETest/AbstractInterview.java
+++ JavaSETest/AbstractInterview.java
@@ -6,6 +6,12 @@ abstract class A{
     public abstract void print();
 }
 
+class C extends A{
+    public void print(){
+        System.out.println("This is C");
+    }
+}
+
 class B extends A{
     private int num = 100;
     public B(int num){
@@ -13,14 +19,17 @@ class B extends A{
         this.num = num;
     }
     public void print(){
-        System.err.println(this.num);
+        System.out.println("This is B");
+        System.out.println(this.num);
     }
 }
+
+
 
 public class AbstractInterview{
     public static void main(String[] args) {
         // new B(30);  //0
-        new B(30).print();
+        new B(30).print(); //0 30
     }
     
 }
