079e2eee9b0f7071a0549ef04a6a728690dedf18
xiaomessi <921721870@qq.com>
2019-07-21 22:52:49 +0800

    add some test

---
+++ JavaTest/src/com/xiaoaxiao/test/threadTest/threadlocal_test/ThreadLocalTest1.java
@@ -0,0 +1,13 @@
+package com.xiaoaxiao.test.thread_test.threadlocal_test;
+
+/**
+ * Created by xiaoaxiao on 2019/7/20
+ * Description: ThreadLocal类测试
+ */
+public class ThreadLocalTest1 {
+
+    public static void main(String[] args) {
+
+    }
+
+}
