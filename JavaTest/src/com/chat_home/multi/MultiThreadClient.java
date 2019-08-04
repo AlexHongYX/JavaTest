b756440f8f9b0d9fd730546a4f65af36a2ead709
xiaomessi <921721870@qq.com>
2019-08-04 23:42:29 +0800

    add multi thread test

--- JavaTest/src/com/chat_home/multi/MultiThreadClient.java
+++ JavaTest/src/com/chat_home/multi/MultiThreadClient.java
@@ -7,6 +7,7 @@ package com.xiaoaxiao.chat_home.multi;
  */
 
 import java.io.IOException;
+import java.io.PrintStream;
 import java.net.Socket;
 import java.util.Scanner;
 
@@ -26,18 +27,21 @@ class ReadFromServerThread implements Runnable{
     @Override
     public void run() {
         try {
-            Scanner scanner = new Scanner(this.client.getInputStream());
+            Scanner scanner = new Scanner(client.getInputStream());
             while (true){
                 if (scanner.hasNext()){
-                    System.out.println("从服务器发来的信息为："+scanner.nextLine());
+                    System.out.println("从服务器端接收信息："+scanner.nextLine());
                 }
+                // 若客户端退出，则此线程也应该退出
                 if (client.isClosed()){
                     System.out.println("客户端已关闭");
                     break;
                 }
             }
+            Thread.sleep(500);
+            // 关闭流
             scanner.close();
-        } catch (IOException e) {
+        } catch (Exception e) {
             e.printStackTrace();
         }
     }
@@ -56,6 +60,32 @@ class WriteToServerThread implements Runnable{
 
     @Override
     public void run() {
+        // 获取键盘输入
+        Scanner in = new Scanner(System.in);
+        // 获取客户端输出流
+        PrintStream printStream = null;
+        try {
+            printStream = new PrintStream(client.getOutputStream()
+                ,true,"UTF-8");
+
+            while (true){
+                System.out.println("请输入要发送的信息..");
+                String strToServer;
+                if (in.hasNext()){
+                    strToServer = in.nextLine();
+                    printStream.println(strToServer);
+                    if (strToServer.equals("byebye")){
+                        System.out.println("关闭客户端");
+                        in.close();
+                        printStream.close();
+                        client.close();
+                        break;
+                    }
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
     }
 }
@@ -67,6 +97,10 @@ public class MultiThreadClient {
             Socket client = new Socket("127.0.0.1",6666);
             Thread writeThread = new Thread(new WriteToServerThread(client));
             Thread readThread = new Thread(new ReadFromServerThread(client));
+
+            // 开启读线程与写线程
+            writeThread.start();
+            readThread.start();
         } catch (IOException e) {
             e.printStackTrace();
         }
--- JavaTest/src/com/chat_home/multi/MultiThreadServer.java
+++ JavaTest/src/com/chat_home/multi/MultiThreadServer.java
@@ -1,9 +1,177 @@
 package com.xiaoaxiao.chat_home.multi;
 
+import java.io.IOException;
+import java.io.PrintStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Scanner;
+import java.util.Set;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 /**
  * Created by xiaoaxiao on 2019/8/3
  * Description: 多线程版本服务器端
- *
  */
+
+class ExecuteClient implements Runnable{
+
+    private static Map<String, Socket> clientMap = new
+            ConcurrentHashMap<>();
+
+    private Socket socket;
+
+    public ExecuteClient(Socket socket) {
+        this.socket = socket;
+    }
+
+    @Override
+    public void run() {
+        try {
+            Scanner scanner = new Scanner(socket.getInputStream());
+            String strFromClient;
+            while (true){
+                if (scanner.hasNext()){
+                    strFromClient = scanner.nextLine();
+                    // 将Windows下的默认换行\r\n中的\r转换为空字符串
+                    Pattern pattern = Pattern.compile("\r");
+                    Matcher matcher = pattern.matcher(strFromClient);
+                    strFromClient = matcher.replaceAll("");
+
+                    // 注册流程
+                    // userName：用户名
+                    if (strFromClient.startsWith("userName")){
+                        // 先将字符串分割，将用户名传入对应函数
+                        String userName = strFromClient.split("\\:")[1];
+                        registerUser(userName);
+                        continue;
+                    }
+                    // 群聊流程
+                    // G：群聊内容
+                    if (strFromClient.startsWith("G")){
+                        String groupMsg = strFromClient.split("\\:")[1];
+                        groupChat(groupMsg);
+                        continue;
+                    }
+
+                    // 私聊流程
+                    // P：用户名-私聊内容
+                    if (strFromClient.startsWith("P")){
+                        String userName = strFromClient.split("\\:")[1]
+                                        .split("-")[0];
+                        String privateMsg = strFromClient.split("\\:")[1]
+                                        .split("-")[1];
+                        privateChat(userName,privateMsg);
+                        continue;
+                    }
+
+                    // 用户退出
+                    // 用户名：byebye
+                    if (strFromClient.contains("byebye")){
+                        // 由于不知道具体是哪个用户说的byebye，因此需要
+                        // 通过Socket来找到对应的userName，并将此对象从
+                        // Map中删除
+                        String username = null;
+                        Set<Map.Entry<String,Socket>> entrySet =
+                                clientMap.entrySet();
+                        Iterator<Map.Entry<String,Socket>> iterator =
+                                entrySet.iterator();
+                        while (iterator.hasNext()){
+                            Map.Entry<String,Socket> entry =
+                                    iterator.next();
+                            if (entry.getValue().equals(this.socket)){
+                                username = entry.getKey();
+                                break;
+                            }
+                        }
+                        clientMap.remove(username);
+                        System.out.println("用户"+username+"已下线");
+                        System.out.println("当前群聊人数为："+clientMap.size()+"人");
+                        continue;
+                    }
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    // 注册
+    private void registerUser(String userName){
+        System.out.println("用户"+userName+"上线啦");
+        // 将用户信息保存在Map中
+        // 每个socket都对应的是一个线程
+        clientMap.put(userName,this.socket);
+        System.out.println("当前群聊人数为："+clientMap.size()+"人");
+        // 添加成功后，服务器端向客户端返回信息
+        try {
+            PrintStream printStream = new PrintStream(socket.getOutputStream()
+                ,true,"UTF-8");
+            printStream.println("用户注册成功");
+            // printStream.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    // 群聊
+    private void groupChat(String groupMsg){
+        Set<Map.Entry<String,Socket>> entrySet =
+                clientMap.entrySet();
+        Iterator<Map.Entry<String,Socket>> iterator =
+                entrySet.iterator();
+        while (iterator.hasNext()){
+            Map.Entry<String,Socket> entry = iterator.next();
+            Socket socket = entry.getValue();
+            try {
+                PrintStream printStream = new PrintStream(socket.getOutputStream()
+                    ,true,"UTF-8");
+                printStream.println("群聊信息："+groupMsg);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    // 私聊
+    private void privateChat(String userName,String privateMsg){
+        Socket socket = clientMap.get(userName);
+        try {
+            PrintStream printStream = new PrintStream(socket.getOutputStream()
+                ,true,"UTF-8");
+            printStream.println("私聊信息为"+privateMsg);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
+
 public class MultiThreadServer {
+
+
+
+    public static void main(String[] args) {
+        // 使用线程池接收多个客户端的连接请求
+        ExecutorService executorService =
+                    Executors.newFixedThreadPool(20);
+        try {
+            ServerSocket serverSocket = new ServerSocket(6666);
+            for (int i = 0; i < 20; i++) {
+                System.out.println("等待客户端连接");
+                Socket socket = serverSocket.accept();
+                System.out.println("有新的客户端连接，端口号为："
+                        +socket.getPort());
+                executorService.submit(new ExecuteClient(socket));
+            }
+            executorService.shutdown();
+            serverSocket.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
