46b0cee7d0165d386fb6cd92a8bef9fd3f17bdcb
xiaomessi <921721870@qq.com>
2019-08-27 23:36:02 +0800

    add maven test

---
+++ JavaWeb/Maven/maven-test-idea/src/main/java/com/xiaoaxiao/hello/HelloApp.java
@@ -0,0 +1,12 @@
+package com.xiaoaxiao.hello;
+
+/**
+ * Created by xiaoaxiao on 2019/8/18
+ * Description:
+ */
+public class HelloApp {
+
+    public static void main(String[] args) {
+        System.out.println("Hello maven");
+    }
+}
