c7821784b6889428399a47fe73d5018662e2adfa
xiaomessi <921721870@qq.com>
2019-10-08 22:22:54 +0800

    add project chat_room

---
+++ Project/chat_home/src/com/xiaoaxiao/multi/MultiThreadServer.java
@@ -0,0 +1,351 @@
+package com.xiaoaxiao.multi;
+
+import java.io.IOException;
+import java.io.PrintStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Scanner;
+import java.util.Set;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+/**
+ * Created by xiaoaxiao on 2019/8/3
+ * Description: 多线程版本服务器端
+ */
+
+// 使用线程实现服务端和客户端通信的业务处理
+class ExecuteClient implements Runnable{
+
+    // 所有线程共享
+    // 维护所有的连接到服务端的客户端对象
+    // ConcurrentHashMap是线程安全的，HashMap是线程不安全的所以不能用
+    private static Map<String, Socket> clientMap = new
+            ConcurrentHashMap<>();
+
+    private Socket socket;
+
+    // 设置一个boolean值代表当前socket只能注册一个用户，再注册就提示错误
+    private boolean isRegisted = false;
+
+    public ExecuteClient(Socket socket) {
+        this.socket = socket;
+    }
+
+    @Override
+    public void run() {
+        try {
+            Scanner scanner = new Scanner(socket.getInputStream());
+            String strFromClient;
+            while (true){
+                if (scanner.hasNext()){
+                    strFromClient = scanner.next();
+//                    // 将Windows下的默认换行\r\n中的\r转换为空字符串
+//                    Pattern pattern = Pattern.compile("\r");
+//                    Matcher matcher = pattern.matcher(strFromClient);
+//                    strFromClient = matcher.replaceAll("");
+
+                    // 注册流程
+                    // userName：用户名     strFromClient.startsWith("userName:")&&
+                    if (strFromClient.matches("userName:[a-zA-Z0-9]{1,10}")){
+                        // 先将字符串分割，将用户名传入对应函数
+                        String userName = strFromClient.split(":")[1];
+                        // 注册前先判断该username是否已存在，若已存在则提示"该用户名已存在"
+//                        if(hasTheOne(userName)){
+//                            String message = "*********************************************\n" +
+//                                    "**当前用户名已存在，请重新注册(输入 userName:<username>：进行注册) **\n"+
+//                                    "*********************************************\n";
+//                            sendMessage(this.socket,message,true);
+//                        }else{
+//                            // 如果当前名字不存在则正常注册即可
+//                            registerUser(userName);
+//                        }
+                        registerUser(userName);
+                        continue;
+                    }else{
+                        // 必须要先注册，只有注册成功了，在Map中能找到对应的username了
+                        // 才能继续进行之后的操作，否则提示"请先注册"
+                        String username = findUsername();
+                        if(username==null){
+                            String message = "*********************************************\n" +
+                                    "**未注册或格式错误，请先注册，输入 userName:<username>：进行注册(名字长度为1-10位字母与数字的组合<字母大小写均可>) **\n"+
+                                    "*********************************************\n";
+                            sendMessage(this.socket,message,true,false);
+                        }else{
+                            // 群聊流程
+                            // G：群聊内容   strFromClient.startsWith("G:")
+                            // 内容的正则可以是包括中文、标点、数字、字母在内的任意字符
+                            // 群聊内容是第一个":"后的内容，也不能用[1]表示(万一群聊内容中也有":")
+                            if (strFromClient.matches("G:[\\u4e00-\\u9fa5\\w\\W]{0,100}")){
+                                // 直接将群聊前面的"G:"删掉，后面的即为群聊内容
+                                String groupMsg = strFromClient.substring(2);
+                                groupChat(groupMsg);
+                                continue;
+                            }
+
+                            // 私聊流程
+                            // P：用户名-私聊内容   strFromClient.startsWith("P:")
+                            // 私聊内容是第一个"-"后所有的字符串，如果后面字符串中包括"-"，只用一个[1]就不对了
+                            else if (strFromClient.matches("P:[a-zA-Z0-9]{1,10}-[\\u4e00-\\u9fa5\\w\\W]{0,100}")){
+                                // 将前面的"P:"直接删掉
+                                String str = strFromClient.substring(2);
+                                // 此时变成了"用户名-私聊内容"，用户名肯定是第一个"-"前的内容
+                                String userName = str.split("-")[0];
+                                // 将第一个"-"以及前面的内容都删掉(用户名)
+                                int index = str.indexOf("-");
+                                str = str.substring(index+1);
+                                String privateMsg = str;
+                                privateChat(userName,privateMsg);
+                                continue;
+                            }
+
+                            // 用户退出
+                            // 用户名：byebye
+                            else if (strFromClient.equals("byebye")){
+                                byebye();
+                                continue;
+                            }
+
+                            // 给某个用户发送文件    strFromClient.matches("File$[a-zA-Z0-9]{1,10}$[\\u4e00-\\u9fa5\\w\\W]{0,100}")
+                            // File$用户名$文件内容字符串
+                            else if(strFromClient.startsWith("File")){
+//                                System.out.println(strFromClient);
+                                //  该段文本中第一个$与第二个$之间的是username
+                                // 注意此处的$是需要转义的
+                                String userName = strFromClient.split("\\$")[1];
+                                System.out.println(userName);
+                                // 第二个$之后的所有文本都是fileContent，万一内容中也包含了$，只去个[2]不合适
+                                // 先将前面无用的"File$"去掉
+                                String str = strFromClient.substring(5);
+                                // 将得到的"用户名$文件内容字符串"按照$来拆分(先找到第一个$的位置)
+                                int index = str.indexOf("$");
+                                // 将第一个$之前的内容全部删掉
+                                str = str.substring(index+1);
+                                // 此时的str即为文件内容字符串
+                                String fileContent = str;
+                                System.out.println(fileContent);
+                                sendFile(userName,fileContent);
+                                continue;
+                            }
+
+                            // 如果输入的不是正确的消息，服务器端需要给客户端返回一条"帮助文档"
+                            else{
+                                String message = "*********************************************\n" +
+                                        "**输入错误，请仔细阅读帮助文档**\n"+
+                                        "**输入 G:<groupMessage>：发送群聊信息(发送消息不超过100个字符<任意字符>且不能为空)**\n" +
+                                        "**输入 P:<username>-<privateMessage>：给username发送私聊信息(发送消息不超过100个字符<任意字符>且不能为空)**\n" +
+                                        "**输入 byebye：退出程序**\n" +
+                                        "*********************************************\n";
+                                sendMessage(this.socket,message,true,false);
+                            }
+                        }
+
+                    }
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * 私发文件
+     * @param userName  目标用户
+     * @param fileContent 文件的二进制内容字符串
+     */
+    private void sendFile(String userName, String fileContent) {
+        if(hasTheOne(userName)){
+            // 判断一下是不是在自己给自己发文件——疯了
+            if(userName.equals(findUsername())){
+                sendMessage(this.socket,"你给自己发文件是疯了？",true,false);
+            }else{
+                // 当个人吧，给一个存在的人发文件(正常操作)
+                Socket client = clientMap.get(userName);
+                // 若发的是文件将isFile设置为true
+                sendMessage(client,"给您发送了一个文件："+fileContent,false,true);
+            }
+        }else{
+            sendMessage(this.socket,"该用户不存在!",true,false);
+        }
+    }
+
+    /**
+     * 在Map中查看有无该用户(根据username)
+     * @param userName 该用户的username
+     * @return
+     */
+    private boolean hasTheOne(String userName){
+        if(clientMap.containsKey(userName)){
+            return true;
+        }
+        return false;
+    }
+
+    /**
+     * 根据当前socket(this.socket)在Map中寻找对应用户的username
+     * @return  该用户的username
+     */
+    private String findUsername(){
+        String username = null;
+        Set<Map.Entry<String,Socket>> entrySet =
+                clientMap.entrySet();
+        Iterator<Map.Entry<String,Socket>> iterator =
+                entrySet.iterator();
+        while (iterator.hasNext()){
+            Map.Entry<String,Socket> entry =
+                    iterator.next();
+            if (entry.getValue().equals(this.socket)){
+                username = entry.getKey();
+                break;
+            }
+        }
+        return username;
+    }
+
+    /**
+     * 专门将发送消息封装为一个函数
+     *  @param socket   目标对象的socket
+     *  @param message   发送的内容
+     *  @param isHidden   若为帮助文档或未注册或当前名字已存在，只显示message即可
+     *  @param isFile   是否是在发送文件，若是在发送文件则"用户"前面再加上File，这样客户端看到是以File开头的就知道发送的是文件了
+     */
+    private void sendMessage(Socket socket,String message,boolean isHidden,boolean isFile){
+        try {
+            PrintStream printStream = new PrintStream(socket.getOutputStream()
+                    ,true,"UTF-8");
+
+            // 若isUserHelp为真则只需要打印message，否则还需要打印username
+            if(isHidden){
+                printStream.println(message);
+            }else if(isFile){
+                // 若发送的是文件，则在"用户"前多加一个File，让客户端就能判断出是否是文件了
+                String username = findUsername();
+                printStream.println("File用户"+username+message);
+            }else{
+                String username = findUsername();
+                printStream.println("用户"+username+message);
+            }
+            // printStream.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * 以userName为key注册当前用户
+     * @param userName 客户端传来的username
+     */
+    private void registerUser(String userName){
+        // 先判断当前用户名时候已经被注册了
+        if(hasTheOne(userName)){
+            String message = "*********************************************\n" +
+                    "**当前用户名已存在，请重新注册(输入 userName:<username>：进行注册) **\n"+
+                    "*********************************************\n";
+            sendMessage(this.socket,message,true,false);
+            return ;
+        }
+        if(this.isRegisted){
+            String message = "*********************************************\n" +
+                    "**当前用户已注册，不可重复注册，请输入其他命令**\n"+
+                    "*********************************************\n";
+            sendMessage(this.socket,message,true,false);
+            return ;
+        }
+        System.out.println("用户"+userName+"上线啦");
+        // 将用户信息保存在Map中
+        // 每个socket都对应的是一个线程
+        clientMap.put(userName,this.socket);
+        // 将isRegisted设置为true，说明当前socket已经注册了一个用户了，不能再注册其他用户
+        isRegisted = true;
+        System.out.println("当前群聊人数为："+clientMap.size()+"人");
+        // 添加成功后，服务器端向客户端返回信息
+        sendMessage(this.socket,"注册成功",false,false);
+    }
+
+    /**
+     * 发送一条群聊信息
+     * @param groupMsg  具体的群聊信息
+     */
+    private void groupChat(String groupMsg){
+        Set<Map.Entry<String,Socket>> entrySet =
+                clientMap.entrySet();
+        Iterator<Map.Entry<String,Socket>> iterator =
+                entrySet.iterator();
+        while (iterator.hasNext()){
+            Map.Entry<String,Socket> entry = iterator.next();
+            Socket client = entry.getValue();
+//            try {
+//                PrintStream printStream = new PrintStream(client.getOutputStream()
+//                    ,true,"UTF-8");
+//                printStream.println("群聊信息："+groupMsg);
+//            } catch (IOException e) {
+//                e.printStackTrace();
+//            }
+            if(client.equals(this.socket)){
+                continue;
+            }
+            sendMessage(client,"发送了一条群聊消息："+groupMsg,false,false);
+        }
+    }
+
+    /**
+     * 发送一条私聊信息
+     * @param userName  具体的目标私聊对象
+     * @param privateMsg    具体的私聊信息
+     */
+    private void privateChat(String userName,String privateMsg){
+        // 先判断目标用户是否存在，若存在则发送
+        // 若不存在则给当前用户提示"该用户不存在"
+        if(hasTheOne(userName)){
+            // 判断一下是不是在自己给自己发消息——疯了
+            if(userName.equals(findUsername())){
+                sendMessage(this.socket,"你给自己发消息是疯了？",true,false);
+            }else{
+                // 当个人吧，给一个存在的人发消息(正常操作)
+                Socket client = clientMap.get(userName);
+                sendMessage(client,"给您发送了一条私聊消息："+privateMsg,false,false);
+            }
+        }else{
+            sendMessage(this.socket,"该用户不存在!",true,false);
+        }
+
+    }
+
+    /**
+     * 用户退出
+     */
+    private void byebye(){
+        // 由于不知道具体是哪个用户说的byebye，因此需要
+        // 通过Socket来找到对应的userName，并将此对象从
+        // Map中删除
+        String username = findUsername();
+        clientMap.remove(username);
+        System.out.println("用户"+username+"已下线");
+        System.out.println("当前群聊人数为："+clientMap.size()+"人");
+    }
+}
+
+public class MultiThreadServer {
+    public static void main(String[] args) {
+        // 使用线程池接收多个客户端的连接请求
+        ExecutorService executorService =
+                    Executors.newFixedThreadPool(20);
+        try {
+            ServerSocket serverSocket = new ServerSocket(6666);
+            for (int i = 0; i < 20; i++) {
+                System.out.println("等待客户端连接");
+                Socket socket = serverSocket.accept();
+                System.out.println("有新的客户端连接，端口号为："
+                        +socket.getPort());
+                executorService.submit(new ExecuteClient(socket));
+            }
+            executorService.shutdown();
+            serverSocket.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ Project/chat_home_plus/chat-room-client/src/main/java/com/xiaoaxiao/final_edition/Client.java
@@ -0,0 +1,241 @@
+package com.xiaoaxiao.final_edition;
+
+/**
+ * Created by xiaoaxiao on 2019/8/3
+ * Description: 多线程版本客户端
+ * 读线程+写线程（保证写的同时可以读）
+ */
+
+import java.io.*;
+import java.net.Socket;
+import java.util.Scanner;
+
+/**
+ * 读取服务器信息线程
+ */
+class ReadFromServerThread implements Runnable {
+
+    // 无论是读线程还是写线程都需要同一个Socket
+    // 通过构造方法将主函数中的socket传入
+    private Socket client;
+
+    public ReadFromServerThread(Socket client) {
+        this.client = client;
+    }
+
+    @Override
+    public void run() {
+        try {
+
+            while (true) {
+                // 读取线程必须是读每一条数据都重新创建一个ObjectInputStream
+                //TODO
+                ObjectInputStream ois = new ObjectInputStream(new BufferedInputStream(this.client.getInputStream()));
+                Object obj = ois.readObject();
+                MyData myData = (MyData) obj;
+//                System.out.println(myData.getFunction());
+//                System.out.println(myData.getMessage());
+//                System.out.println(myData.getMyFile());
+                // 若发送的是普通消息，直接打印即可
+                if(myData.getFunction().equals("SendMessage")){
+                    System.out.println(myData.getMessage());
+                }else if(myData.getFunction().equals("SendFile")){
+                    // 若发送的是文件，除了打印信息外，还要将文件置入指定位置
+                    System.out.println(myData.getMessage());
+                    copy_file(myData.getMyFile());
+                    System.out.println("接收文件"+myData.getMyFile().getFile_name()+"成功，请在F盘test目录下查看");
+                }else if(myData.getFunction().equals("CloseSocket")){
+                    System.out.println("客户端已关闭");
+                    this.client.close();
+                    break;
+                }
+
+//                // 若客户端退出，则此线程也应该退出
+//                if (client.isClosed()) {
+//                    // 若客户端已经关闭，则也向服务端发送该客户端被关闭的消息(与byebye相同)
+//                    break;
+//                }
+
+            }
+        }catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void copy_file(MyFile myFile){
+        String fileName = myFile.getFile_name();
+        File file = new File("F:"+File.separator+"test"+File.separator+fileName);
+        if (!file.getParentFile().exists()){
+            file.getParentFile().mkdirs();
+        }
+        try {
+            OutputStream os = new FileOutputStream(file);
+            os.write(myFile.getFile_data());
+            os.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
+
+/**
+ * 将信息发送给服务器线程
+ */
+class WriteToServerThread implements Runnable {
+
+    private Socket client;
+
+    public WriteToServerThread(Socket client) {
+        this.client = client;
+    }
+
+    @Override
+    public void run() {
+        // 获取键盘输入
+        Scanner in = new Scanner(System.in);
+        // 使用对象流向服务端进行输出
+        ObjectOutputStream oos = null;
+        // 设定一个boolean值代表必须先进行注册
+        boolean isRegisted = false;
+        try {
+            oos = new ObjectOutputStream(this.client.getOutputStream());
+
+            while (true) {
+                System.out.println("请输入要发送的信息..");
+                String strToServer;
+                MyData myData = new MyData();
+                if (in.hasNext()) {
+                    strToServer = in.nextLine();
+                    // 必须要先注册再干其他的事情
+                    if(strToServer.matches("userName:[a-zA-Z0-9]{1,10}")){
+                        myData.setFunction("register");
+                        myData.setMessage(strToServer.substring(9));
+                        isRegisted = true;
+                    }else{
+                        // 只有当已经注册了，才可以干其他的事情(发文件)
+                        if(isRegisted){
+                            // 群聊
+                            if(strToServer.matches("G:[\\u4e00-\\u9fa5\\w\\W]{0,100}")){
+                                myData.setFunction("GroupChat");
+                                myData.setMessage(strToServer.substring(2));
+                            }
+                            // 私聊
+                            else if (strToServer.matches("P:[a-zA-Z0-9]{1,10}-[\\u4e00-\\u9fa5\\w\\W]{0,100}")){
+                                // 将前面的"P:"直接删掉
+                                String str = strToServer.substring(2);
+                                // 此时变成了"用户名-私聊内容"，用户名肯定是第一个"-"前的内容
+                                String userName = str.split("-")[0];
+                                // 将第一个"-"以及前面的内容都删掉(用户名)
+                                int index = str.indexOf("-");
+                                str = str.substring(index+1);
+                                String privateMsg = str;
+                                myData.setFunction("PrivateChat");
+                                myData.setMessage(privateMsg);
+                                myData.setDestUser(userName);
+                            }
+                            // 如果输入的是文件发送相关
+                            //  File$userName$filePath
+                            else if(strToServer.startsWith("File")){
+                                String filePath = strToServer.split("\\$")[2];
+                                String userName = strToServer.split("\\$")[1];
+                                int index = strToServer.lastIndexOf(File.separator);
+                                String fileName = strToServer.substring(index+1);
+
+                                myData.setFunction("SendFile");
+                                myData.setDestUser(userName);
+                                MyFile myFile = new MyFile();
+                                myFile.setFile_name(fileName);
+
+                                byte[] b = load_file(filePath);
+                                // 如果b为null说明文件不存在/加载不成功
+                                if(b == null){
+                                    continue;
+                                }
+                                myFile.setFile_data(b);
+                                myData.setMyFile(myFile);
+                            }
+                            // 如果输入的是结束语
+                            // byebye
+                            else if (strToServer.equals("byebye")){
+                                myData.setFunction("byebye");
+                            }
+                            else {
+                                myData.setFunction("wrong");
+                            }
+                        }else {
+                            myData.setFunction("wrong");
+                        }
+                    }
+
+
+                    // 必须先给服务器发送信息，再break
+                    oos.writeObject(myData);
+                    oos.flush();
+
+                    if (strToServer.equals("byebye")){
+                        in.close();
+                        break;
+                    }
+
+//                    if (myData.getFunction().equals("byebye")) {
+//                        System.out.println("客户端已关闭");
+//
+//                    }
+
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * 根据文件路径将文件保存为一个字节数组返回
+     * @param filePath  文件路径
+     * @return  保存字节流数据的字节数组
+     */
+    private byte[] load_file(String filePath){
+        File file = new File(filePath);
+        if (!file.exists()){
+            System.out.println("文件不存在");
+            return null;
+        }
+        try {
+            InputStream is = new FileInputStream(file);
+            byte[] b = new byte[(int) file.length()];
+            is.read(b);
+            is.close();
+            return b;
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+}
+
+public class Client {
+
+    public static void main(String[] args) {
+        try {
+            Socket client = new Socket("47.112.23.227", 6666);
+            Thread writeThread = new Thread(new WriteToServerThread(client));
+            Thread readThread = new Thread(new ReadFromServerThread(client));
+
+            // 先打印提示信息
+            String message = "******************************************************************************************************************\n" +
+                    "                                               输入前请仔细阅读<帮助文档>\n"+
+                    "       输入 userName:<username>：进行注册\n"+
+                    "       输入 G:<groupMessage>：发送群聊信息(发送消息不超过100个字符<任意字符>且不能为空)\n" +
+                    "       输入 P:<username>-<privateMessage>：给username发送私聊信息(发送消息不超过100个字符<任意字符>且不能为空)\n" +
+                    "       输入 File$<username>$<FilePath>：给username发送一个文件，文件地址为FilePath\n"+
+                    "       输入 byebye：退出程序\n" +
+                    "******************************************************************************************************************\n";
+            System.out.println(message);
+            // 开启读线程与写线程
+            writeThread.start();
+            readThread.start();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
