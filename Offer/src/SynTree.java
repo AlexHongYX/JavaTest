e8da7545ade251e0db4cd511f7405ff4b5f7244a
xiaomessi <921721870@qq.com>
2019-10-09 23:29:03 +0800

    add some offer test

---
+++ Offer/src/SynTree.java
@@ -0,0 +1,92 @@
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+/**
+ * Created by xiaoaxiao on 2019/10/9
+ * Description: 判断一棵二叉树是否为对称的
+ */
+
+class TreeNode {
+    int val = 0;
+    TreeNode left = null;
+    TreeNode right = null;
+
+    public TreeNode(int val) {
+        this.val = val;
+
+    }
+
+    @Override
+    public String toString() {
+        return String.valueOf(val);
+    }
+}
+
+public class SynTree {
+
+    public static boolean isSymmetrical(TreeNode pRoot)
+    {
+        Queue<TreeNode> queue1 = new LinkedList<>();
+        Queue<TreeNode> queue2 = new LinkedList<>();
+        List<TreeNode> list1 = new ArrayList<>();
+        List<TreeNode> list2 = new ArrayList<>();
+        queue1.offer(pRoot);
+        while(!queue1.isEmpty()){
+            TreeNode node = queue1.poll();
+            if(node!=null){
+                queue1.offer(node.left);
+                queue1.offer(node.right);
+            }
+            list1.add(node);
+        }
+        queue2.offer(pRoot);
+        while(!queue2.isEmpty()){
+            TreeNode node = queue2.poll();
+            if(node!=null){
+                queue2.offer(node.right);
+                queue2.offer(node.left);
+            }
+            list2.add(node);
+        }
+        System.out.println(list1);
+        System.out.println(list2);
+        if (list1.size()!=list2.size()){
+            return false;
+        }
+        for (int i = 0; i < list1.size(); i++) {
+            TreeNode node1 = list1.get(i);
+            TreeNode node2 = list2.get(i);
+            if(node1==null&&node2==null){
+                continue;
+            }else if(node1==null||node2==null){
+                return false;
+            }else {
+                if(node1.val!=node2.val){
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        TreeNode root = new TreeNode(8);
+        TreeNode A = new TreeNode(6);
+        TreeNode B = new TreeNode(10);
+        TreeNode C = new TreeNode(5);
+        TreeNode D = new TreeNode(7);
+        TreeNode E = new TreeNode(9);
+        TreeNode F = new TreeNode(11);
+
+        root.left = A;
+        root.right = B;
+        A.left = C;
+        A.right = D;
+
+        B.right = F;
+
+        System.out.println(isSymmetrical(root));
+    }
+}
---
+++ Offer/src/TreeNextNode.java
@@ -0,0 +1,85 @@
+/**
+ * Created by xiaoaxiao on 2019/10/9
+ * Description: 二叉树中序遍历的下一个结点
+ *  https://www.nowcoder.com/practice/9023a0c988684a53960365b889ceaf5e?tpId=13&tqId=11210&tPage=1&rp=1&ru=/ta/coding-interviews&qru=/ta/coding-interviews/question-ranking
+ */
+
+class TreeLinkNode {
+    int val;
+    TreeLinkNode left = null;
+    TreeLinkNode right = null;
+    TreeLinkNode next = null;
+
+    TreeLinkNode(int val) {
+        this.val = val;
+    }
+
+    @Override
+    public String toString() {
+        return String.valueOf(val);
+    }
+}
+
+public class TreeNextNode {
+    public static TreeLinkNode GetNext(TreeLinkNode pNode) {
+        TreeLinkNode nextNode = null;
+        // 当前为中间结点
+        if (pNode.right != null) {
+            nextNode = pNode.right;
+            while (nextNode.left != null) {
+                nextNode = nextNode.left;
+            }
+            return nextNode;
+        }
+        TreeLinkNode parent = pNode.next;
+        if (parent==null){
+            return null;
+        }
+        // 当前为左子节点
+        if (pNode == parent.left) {
+            return parent;
+        } else {
+            // 当前为右子节点
+            // 看当前父节点是其父节点的右子节点还是左子节点
+            // 左子节点：说明当前节点的父节点的父节点还没走，下一个就该走了
+            // 右子节点：说明当前节点的父节点的父节点已经走过了，继续往上找
+            TreeLinkNode node = parent.next;
+            while(node!=null){
+                if(parent==node.left){
+                    break;
+                }else{
+                    parent = node;
+                    node = parent.next;
+                }
+            }
+            return node;
+        }
+    }
+
+    public static void main(String[] args) {
+        TreeLinkNode root = new TreeLinkNode(8);
+        TreeLinkNode A = new TreeLinkNode(6);
+        TreeLinkNode B = new TreeLinkNode(10);
+        TreeLinkNode C = new TreeLinkNode(5);
+        TreeLinkNode D = new TreeLinkNode(7);
+        TreeLinkNode E = new TreeLinkNode(9);
+        TreeLinkNode F = new TreeLinkNode(11);
+
+        root.left = A;
+        root.right = B;
+        A.next = root;
+        B.next = root;
+        A.left = C;
+        A.right = D;
+        B.left = E;
+        B.right = F;
+        C.next = A;
+        D.next = A;
+        E.next = B;
+        F.next = B;
+
+//        System.out.println(root);
+        TreeLinkNode node = GetNext(D);
+        System.out.println(node);
+    }
+}
